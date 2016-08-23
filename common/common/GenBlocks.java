9bbba329b76ec391bdd5e0babc474b3df5af8818
Justin <Justin@Justin-PC>
2016-08-22 22:58:04 -0500

    adding blockgen class

---
+++ common/common/GenBlocks.java
@@ -0,0 +1,13 @@
+
+public class GenBlocks {
+
+	public GenBlocks() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
