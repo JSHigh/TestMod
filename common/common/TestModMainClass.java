6a55f65d0961aa05e8e0755a70f982aff3837a16
Justin <Justin@Justin-PC>
2016-08-22 22:56:54 -0500

    first commit - local

---
+++ common/common/TestModMainClass.java
@@ -0,0 +1,14 @@
+package common;
+
+public class TestModMainClass {
+
+	public TestModMainClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
